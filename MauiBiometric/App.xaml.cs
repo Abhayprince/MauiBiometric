f6d8031c8a4581117975de5d76e20278ce9f1b6b
Abhay Prince <46496449+Abhayprince@users.noreply.github.com>
2025-05-28 23:00:38 +0530

    Add project files.

---
+++ MauiBiometric/App.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Plugin.Maui.Biometric;
+
+namespace MauiBiometric
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            //return new Window(new AppShell());
+            return new Window(new BiometricAuthPage());
+        }
+
+        public static void SetShell()
+        {
+            App.Current.Windows[0].Page = new AppShell();
+        }
+    }
+    class BiometricAuthPage : ContentPage
+    {
+        protected async override void OnAppearing()
+        {
+            var biometric = BiometricAuthenticationService.Default;
+
+            if (biometric.IsPlatformSupported)
+            {
+                var authenticationRequest = new AuthenticationRequest
+                {
+                    Title = "Unlock to continue",
+                    NegativeText = "Cancel",
+                    Subtitle = "This is subtitle text",
+                    Description = "This is description",
+                    AllowPasswordAuth = true,
+                };
+                AuthenticationResponse response = await biometric.AuthenticateAsync(authenticationRequest, CancellationToken.None);
+
+                if (response.Status == BiometricResponseStatus.Success)
+                {
+                    // Biometric auth success
+                    //await DisplayAlert("Success", "Biometric auth success", "OK");
+                    App.SetShell();
+                    return;
+                }
+                else
+                {
+                    // Error in biometric auth
+                    // or User Cancelled
+                    await DisplayAlert("Failure", response.ErrorMsg ?? "Unknow error in biometric auth", "OK");
+                }
+            }
+        }
+    }
+}
---
+++ MauiBiometric/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Plugin.Maui.Biometric;
+using System.Threading.Tasks;
+
+namespace MauiBiometric
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private async void UnlockBtn_Clicked(object sender, EventArgs e)
+        {
+            var biometric = BiometricAuthenticationService.Default;
+
+            if (biometric.IsPlatformSupported)
+            {
+                var authenticationRequest = new AuthenticationRequest
+                {
+                    Title = "Unlock to continue",
+                    NegativeText = "Cancel",
+                    Subtitle = "This is subtitle text",
+                    Description ="This is description",
+                    AllowPasswordAuth = true,
+                };
+                AuthenticationResponse response = await biometric.AuthenticateAsync(authenticationRequest, CancellationToken.None);
+
+                if(response.Status == BiometricResponseStatus.Success)
+                {
+                    // Biometric auth success
+                    await DisplayAlert("Success", "Biometric auth success", "OK");
+                }
+                else
+                {
+                    // Error in biometric auth
+                    // or User Cancelled
+                    await DisplayAlert("Failure", response.ErrorMsg ?? "Unknow error in biometric auth", "OK");
+                }
+            }
+        }
+    }
+
+}
